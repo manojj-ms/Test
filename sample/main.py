3ffe0f854ff9074d0c48bd8b7c868a5bef2df119
leo phenomenal <leo@phenomenaltech.com>
2021-05-31 17:12:16 +0530

    again changed.py

--- sample/main.py
+++ sample/main.py
@@ -3,17 +3,15 @@ from time import sleep
 print("This is my file to demonstrate best practices.")
 
 def process_data(data):
-    print("Beginning data processing...")
+    print("Beginning data processing")
     modified_data = data + " that has been modified"
     sleep(3)
     print("Data processing finished.")
     return modified_data
 
 def main():
-    data = "My data read from the Web"
+    data = "My data read from the web application"
     print(data)
     modified_data = process_data(data)
     print(modified_data)
 
-if __name__ == "__main__":
-    main()
